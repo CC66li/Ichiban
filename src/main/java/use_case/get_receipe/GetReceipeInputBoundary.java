7caed778395c2b657d579f65081b3cc78a9add78
K <2749079327@qq.com>
2024-12-01 03:03:54 -0500

    1

--- src/main/java/use_case/get_receipe/GetReceipeInputBoundary.java
+++ src/main/java/use_case/get_receipe/GetReceipeInputBoundary.java
@@ -1,7 +1,10 @@
 package use_case.get_receipe;
 
+import org.json.JSONArray;
 import use_case.get_receipe.GetReceipeInputData;
 import use_case.login.LoginInputData;
+
+import java.util.Map;
 
 /**
  * The Get Receipe Use Case.
@@ -13,7 +16,7 @@ public interface GetReceipeInputBoundary {
      * @param getReceipeInputData the input data for this receipt use case
      * @param loginInputData the input data for this use case
      */
-    void execute(GetReceipeInputData getReceipeInputData, LoginInputData loginInputData);
+    JSONArray execute(GetReceipeInputData getReceipeInputData, LoginInputData loginInputData);
 
     /**
      * Executes the switch to receipt generator view use case.
